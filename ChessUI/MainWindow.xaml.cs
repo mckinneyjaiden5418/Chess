8e46332bd614d0034bf129cebf097aa74841512b
mckinneyjaiden5418 <spiderbeary@gmail.com>
2025-04-21 23:02:20 -0400

    Added pieces to initial board.

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessLogic;
 
 namespace ChessUI
 {
@@ -16,9 +17,41 @@ namespace ChessUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] pieceImages = new Image[8, 8];
+
+        private GameState gameState;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
+        }
+
+        private void InitializeBoard()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Image image = new Image();
+                    pieceImages[i, j] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
+        }
+        private void DrawBoard(Board board)
+        {
+            for (int i = 0; i < 8;i++)
+            {
+                for (int j =0; j < 8; j++)
+                {
+                    Piece piece = board[i, j];
+                    pieceImages[i, j].Source = Images.GetImage(piece);
+                }
+            }
         }
     }
 }
