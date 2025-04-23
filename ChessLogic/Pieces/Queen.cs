25d00f56bcf1ead7770a25107458f9cdb5c177eb
mckinneyjaiden5418 <spiderbeary@gmail.com>
2025-04-22 21:17:02 -0400

    Setup move classes - mainly for bishop, rook, and queen.

--- ChessLogic/Pieces/Queen.cs
+++ ChessLogic/Pieces/Queen.cs
@@ -5,6 +5,17 @@
         public override PieceType Type => PieceType.Queen;
         public override Player Color { get; }
 
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.South,
+            Direction.East,
+            Direction.West,
+            Direction.NorthEast,
+            Direction.SouthEast,
+            Direction.NorthWest,
+            Direction.SouthWest
+        };
         public Queen(Player color)
         {
             Color = color;
@@ -15,6 +26,11 @@
             Queen copy = new Queen(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
         }
     }
 }
