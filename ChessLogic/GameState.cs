bf027ea6230fc02ec8418e990721c740d0f0f89a
mckinneyjaiden5418 <spiderbeary@gmail.com>
2025-04-26 21:25:21 -0400

    Finished moves for all pieces (until I add a custom piece later)

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -4,5 +4,22 @@
     {
         public Board Board { get; } = board;
         public Player CurrentPlayer { get; private set; } = player;
+        
+        public IEnumerable<Move> LegalMovesForPiece(Position pos)
+        {
+            if (Board.IsEmpty(pos) || Board[pos].Color != CurrentPlayer)
+            {
+                return Enumerable.Empty<Move>();
+            }
+
+            Piece piece = Board[pos];
+            return piece.GetMoves(pos, board);
+        }
+
+        public void MakeMove(Move move)
+        {
+            move.Execute(Board);
+            CurrentPlayer = CurrentPlayer.Opponent();
+        }
     }
 }
--- ChessLogic/Pieces/King.cs
+++ ChessLogic/Pieces/King.cs
@@ -5,6 +5,18 @@
         public override PieceType Type => PieceType.King;
         public override Player Color { get; }
 
+        private static readonly Direction[] dirs = new Direction[] 
+        { 
+            Direction.North,
+            Direction.South,
+            Direction.West,
+            Direction.East,
+            Direction.NorthWest,
+            Direction.NorthEast,
+            Direction.SouthWest,
+            Direction.SouthEast
+        };
+
         public King(Player color)
         {
             Color = color;
@@ -15,6 +27,32 @@
             King copy = new King(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            foreach (Direction dir in dirs)
+            {
+                Position to = from + dir;
+                
+                if (!Board.IsInside(to))
+                {
+                    continue;
+                }
+
+                if (board.IsEmpty(to) || board[to].Color != Color)
+                {
+                    yield return to;
+                }
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            foreach (Position to in MovePositions(from, board))
+            {
+                yield return new NormalMove(from, to);
+            }
         }
     }
 }
