fd5e5a2506659e917a6e89b6617a4da9ac2f1b29
mckinneyjaiden5418 <spiderbeary@gmail.com>
2025-07-27 21:50:11 -0400

    Fifty move rule added.

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -5,6 +5,8 @@
         public Board Board { get; } = board;
         public Player CurrentPlayer { get; private set; } = player;
         public Result Result { get; private set; } = null;
+
+        private int noCaptureOrPawnMoves = 0;
         
         public IEnumerable<Move> LegalMovesForPiece(Position pos)
         {
@@ -21,8 +23,18 @@
         public void MakeMove(Move move)
         {
             Board.SetPawnSkipPosition(CurrentPlayer, null);
-            move.Execute(Board);
-            CurrentPlayer = CurrentPlayer.Opponent();
+            bool captureOrPawn = move.Execute(Board);
+
+            if (captureOrPawn)
+            {
+                noCaptureOrPawnMoves = 0;
+            }
+            else
+            {
+                noCaptureOrPawnMoves++;
+            }
+
+                CurrentPlayer = CurrentPlayer.Opponent();
             CheckForGameOver();
         }
 
@@ -54,11 +66,22 @@
             {
                 Result = Result.Draw(EndReason.InsufficientMaterial);
             }
+            else if (FiftyMoveRule())
+            {
+                Result = Result.Draw(EndReason.FiftyMoveRule);
+            }
         }
 
         public bool IsGameOver()
         {
             return Result != null;
+        }
+
+        private bool FiftyMoveRule()
+        {
+            int fullMoves = noCaptureOrPawnMoves / 2;
+
+            return fullMoves == 50;
         }
     }
 }
